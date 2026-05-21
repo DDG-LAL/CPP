4df567837cd8fa47aba71ca3676115bdfa073d0b
DDG-LAL <1935088714@qq.com>
2026-05-21 17:47:00 +0800

    vector

--- CPP_Basic/Project4_MyVector/Project4_MyVector/MyVector.h
+++ CPP_Basic/Project4_MyVector/Project4_MyVector/MyVector.h
@@ -34,44 +34,156 @@ namespace MyVector
 		{
 			return _finish;
 		}
+		size_t capacity() const
+		{
+			return _end_of_storage - _start;
+		}
+		size_t size() const
+		{
+			return _finish - _start;
+		}
+		void swap(vector<T>& v)
+		{
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_end_of_storage, v._end_of_storage);
+		}
 
 		vector() //无参构造
-			: _start(nullptr)
-			, _finish(nullptr)
-			, _end_of_storage(nullptr)
+			//: _start(nullptr)
+			//, _finish(nullptr)
+			//, _end_of_storage(nullptr)
 		{}
+		vector(const vector<T>& v) //拷贝构造
+			//: _start(nullptr)
+			//, _finish(nullptr)
+			//, _end_of_storage(nullptr)
+		{
+			reserve(v.capacity());
+			for (auto& e : v)
+				push_back(e);
+		}
+		vector(size_t n, const T& val = T()) //用给定数量的相同对象进行构造
+			//: _start(nullptr)
+			//, _finish(nullptr)
+			//, _end_of_storage(nullptr)
+		{
+			reserve(n);
+			for (size_t i = 0; i < n; ++i)
+				push_back(val);
+		}
+		vector(int n, const T& val = T()) //用给定数量的相同对象进行构造
+			//: _start(nullptr)			  //为了避免参数是(int, int)时调用模板，重载给出更匹配的函数
+			//, _finish(nullptr)
+			//, _end_of_storage(nullptr)
+		{
+			reserve(n);
+			for (int i = 0; i < n; ++i)
+				push_back(val);
+		}
+		template<class InputIterator>
+		vector(InputIterator first, InputIterator last) //迭代器区间构造
+			//: _start(nullptr)
+			//, _finish(nullptr)
+			//, _end_of_storage(nullptr)
+		{
+			while (first != last)
+				push_back(*first++);
+		}
+		vector<T>& operator=(vector<T> tmp) //赋值重载
+		{
+			swap(tmp);
+			return *this;
+		}
 		~vector() //析构
 		{
 			delete[] _start;
 			_start = _finish = _end_of_storage = nullptr;
 		}
-		size_t capacity()
-		{
-			return _end_of_storage - _start;
-		}
-		size_t size()
-		{
-			return _finish - _start;
-		}
 
-		void push_back(const T& val)
+		void reserve(size_t newcapacity) //修改容量
 		{
-			if (_finish == _end_of_storage)
+			if (newcapacity > capacity())
 			{
-				size_t cap = capacity(), sz = size();
-				size_t newcapacity = cap == 0 ? 4 : cap * 2;
+				size_t sz = size();
 				T* tmp = new T[newcapacity];
 				if (_start != nullptr)
 				{
-					memcpy(tmp, _start, sizeof(T) * size());
+					//memcpy(tmp, _start, sizeof(T) * sz); //存在浅拷贝问题
+					for (size_t i = 0; i < sz; ++i)
+						tmp[i] = _start[i]; //利用赋值重载进行深拷贝
 					delete[] _start;
 				}
 				_start = tmp;
 				_finish = _start + sz; //指针_finish更新
 				_end_of_storage = _start + newcapacity; //指针_end_of_storage更新
 			}
-			*_finish = val;
+		}
+		void resize(size_t newsize, const T& val = T()) //修改长度
+		{							//缺省值使用匿名对象，内置类型也有默认构造函数，也可以正常使用
+			if (newsize <= size())
+				_finish = _start + newsize;
+			else
+			{
+				reserve(newsize);
+				while (_finish < _start + newsize)
+					*_finish++ = val;
+			}
+		}
+
+		void push_back(const T& val) //尾插
+		{
+			//if (_finish == _end_of_storage)
+			//{
+			//	size_t cap = capacity();
+			//	reserve(cap == 0 ? 4 : cap * 2);
+			//}
+			//*_finish = val;
+			//++_finish;
+			insert(_finish, val);
+		}
+		void pop_back() //尾删
+		{
+			if (_finish != _start)
+				--_finish;
+		}
+
+		void insert(iterator pos, const T& val) //在pos的前一个位置插入
+		{
+			assert(pos <= _finish);
+			assert(pos >= _start);
+			if (_finish == _end_of_storage)
+			{
+				size_t cap = capacity(), len = pos - _start; //扩容后_start转移，导致pos失效，因此预先记录pos
+				reserve(cap == 0 ? 4 : cap * 2);
+				pos = _start + len; //扩容后_start转移，导致pos失效，因此更新pos
+			}
+			iterator end = _finish - 1;
+			while (end >= pos)
+			{
+				*(end + 1) = *end;
+				--end;
+			}
+			*pos = val;
 			++_finish;
+		}
+		iterator erase(iterator pos) //删除pos位置数据
+		{
+			assert(pos < _finish);
+			assert(pos >= _start);
+			iterator it = pos + 1;
+			while (it < _finish)
+			{
+				*(it - 1) = *it;
+				++it;
+			}
+			--_finish;
+			return pos; //返回pos位置，用于解决迭代器失效
+		}
+
+		void clear()
+		{
+			_finish = _start;
 		}
 
 		T& operator[](size_t pos)
@@ -85,8 +197,8 @@ namespace MyVector
 			return *(_start + pos);
 		}
 	private:
-		iterator _start;
-		iterator _finish;
-		iterator _end_of_storage;
+		iterator _start = nullptr;
+		iterator _finish = nullptr;
+		iterator _end_of_storage = nullptr;
 	};
 }
--- CPP_Basic/Project4_MyVector/Project4_MyVector/MyVectorTest.cpp
+++ CPP_Basic/Project4_MyVector/Project4_MyVector/MyVectorTest.cpp
@@ -1,7 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include"MyVector.h"
+#include<string>
 using namespace MyVector;
+using std::string;
+
+template<class T>
+void print(const vector<T>& v)
+{
+	for (const auto& e : v)
+		cout << e << " ";
+	cout << endl << endl;
+}
+
 
 int main()
 {
@@ -13,18 +24,92 @@ int main()
 	v1.push_back(4);
 	v1.push_back(5);
 	v1.push_back(6);
+	cout << "v1:\n";
 	for (size_t i = 0; i < v1.size(); ++i)
 		cout << v1[i] << " ";
-	cout << endl << endl;
+	cout << endl;
 
 	vector<int>::iterator it = v1.begin();
 	while (it != v1.end())
 		cout << *it++ << " ";
-	cout << endl << endl;
+	cout << endl;
 
-	for (auto e : v1)
-		cout << e << " ";
-	cout << endl << endl;
+	print(v1);
+	v1.pop_back();
+	print(v1);
+	v1.pop_back();
+	v1.pop_back();
+	v1.pop_back();
+	v1.pop_back();
+	v1.pop_back();
+	v1.pop_back();
+	print(v1);
+
+	vector<int> v2;
+	v2.resize(10);
+	cout << "   vector<int> v2: "; print(v2);
+	vector<int*> v3;
+	v3.resize(5);
+	cout << "  vector<int*> v3: "; print(v3);
+	vector<string> v4;
+	v4.resize(5);
+	cout << "vector<string> v4: "; print(v4);
+	v4.resize(7, string("abc")); //匿名对象
+	cout << "vector<string> v4: "; print(v4);
+	v4.resize(9, "snt"); //隐式类型转换
+	cout << "vector<string> v4: "; print(v4);
+
+	vector<int> v5;
+	cout << "v5:\n";
+	v5.push_back(0);
+	v5.push_back(1);
+	v5.push_back(2);
+	print(v5);
+	vector<int>::iterator pos = v5.begin();
+	v5.insert(pos + 1, 6); pos = v5.begin();
+	v5.insert(pos + 3, 6); pos = v5.begin();
+	print(v5);
+
+	v5.erase(pos + 2); pos = v5.begin();
+	print(v5);
+	v5.erase(pos); pos = v5.begin();
+	print(v5);
+
+	vector<int> v6;
+	cout << "v6:\n";
+	v6.push_back(1);
+	v6.push_back(1);
+	v6.push_back(2);
+	v6.push_back(2);
+	v6.push_back(3);
+	v6.push_back(6);
+	print(v6);
+	it = v6.begin();
+	while (it != v6.end())
+	{
+		if (*it % 2 == 0) //检查是否为偶数，是偶数则删除
+			it = v6.erase(it); //通过返回值解决迭代器失效
+		else
+			++it;
+	}
+	print(v6);
+
+	vector<int> v7(v6); //拷贝构造
+	cout << "v7:\n";
+	print(v7);
+	v7 = v2; //赋值重载
+	print(v7);
+
+	vector<int> v8(v7.begin(), v7.begin() + 5); //迭代器区间构造
+	cout << "v8:\n";
+	print(v8);
+
+	vector<int> v9(10, 1); //用给定数量的相同对象进行构造
+	cout << "v9:\n";
+	print(v9);
+	vector<string> v10(10, "sn"); //用给定数量的相同对象进行构造
+	cout << "v10:\n";
+	print(v10);
 
 	return 0;
 }
